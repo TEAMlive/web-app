3d6582a0c2897cd015d22b01922983fbd66fc3c5
now-n <n.ermak08@yandex.by>
2025-04-03 18:38:13 +0300

    client deleted (front)

--- app/client/src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    REACT_APP_API_URL: string;
-    NODE_ENV: 'development' | 'production' | 'test';
-    PORT: string;
-    GENERATE_SOURCEMAP: string;
-  }
-}
