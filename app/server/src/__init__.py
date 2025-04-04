9a119915eb3f529551cad2e4dc4f68ce3a7fff77
now-n <n.ermak08@yandex.by>
2025-04-04 20:18:02 +0300

    fix for API access bug due to incorrect proxying

--- app/server/src/__init__.py
+++ app/server/src/__init__.py
@@ -1,11 +1,21 @@
+from http import HTTPStatus
+
 from fastapi import APIRouter
 
-from .api import router as api_router
+from .api import v1_router as v1
+from .schemas import ErrorResponse
 
 
-router: APIRouter = APIRouter()
-router.include_router(
-    router=api_router
+router: APIRouter = APIRouter(
+    responses={
+        HTTPStatus.BAD_REQUEST.value: {"model": ErrorResponse},
+        HTTPStatus.UNAUTHORIZED.value: {"model": ErrorResponse},
+        HTTPStatus.FORBIDDEN.value: {"model": ErrorResponse},
+        HTTPStatus.NOT_FOUND.value: {"model": ErrorResponse},
+        HTTPStatus.CONFLICT.value: {"model": ErrorResponse},
+        HTTPStatus.INTERNAL_SERVER_ERROR.value: {"model": ErrorResponse},
+    },
 )
+router.include_router(v1)
 
 __all__ = ['router']
